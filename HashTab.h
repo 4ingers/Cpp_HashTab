5ad065cef7fff4537dee0552c5c7a62c33efd3ac
4ingers <38619177+4ingers@users.noreply.github.com>
2018-12-11 18:29:29 +0300

    Add files via upload

--- HashTab.h
+++ HashTab.h
@@ -99,7 +99,8 @@ public:
 private:
 
 // Хэш-таблица: вектор списков с узлами
-// (для решения проблемы коллизии: метод цепочек)	std::vector<std::list<Node<TKey, TData>>> tableStore;
+// (для решения проблемы коллизии: метод цепочек
+	std::vector<std::list<Node<TKey, TData>>> tableStore;
 
 // Хранение произведённых действий: вектор идентификаторов
 	std::stack<int> cancelArr;
